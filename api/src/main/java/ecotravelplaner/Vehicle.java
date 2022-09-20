7fb8df775c9b752cf03fa54b50733db4dfe8ceb1
AlexanderBratic <98606025+AlexanderBratic@users.noreply.github.com>
2022-09-20 15:25:11 +0200

    added transport options

---
+++ api/src/main/java/ecotravelplaner/Vehicle.java
@@ -0,0 +1,17 @@
+package ecotravelplaner;
+
+public enum Vehicle {
+    CAR,
+    BIKE,
+    TRAIN,
+    AIRPLANE,
+    MC,
+    ESCOOTER,
+    EBIKE,
+    TRAM,
+    SUBWAY,
+    MOPED,
+    BUS,
+    WALKING,
+    NONE
+}
--- api/src/main/java/ecotravelplaner/contollers/TransportController.java
+++ api/src/main/java/ecotravelplaner/contollers/TransportController.java
@@ -1,5 +1,7 @@
 package ecotravelplaner.contollers;
 
+import ecotravelplaner.Vehicle;
+import ecotravelplaner.Model;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestHandler;
@@ -12,7 +14,7 @@ public class TransportController {
     @GetMapping(path = "/transport", produces = MediaType.APPLICATION_JSON_VALUE)
     public Transport getTransport() {
 
-        return new Transport("Car");
+        return new Transport(Vehicle.CAR, Model.SEDAN, 15.7, 149.2);
     }
 
     // the api endpoint for /, that receives and returns a json object of hello world
