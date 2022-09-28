75947f24d3a7151e44b17fd650f4ba839e8bd86e
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2022-09-28 23:32:23 +0200

    reworked the model for selecting vehicles and made the advanced part functional

--- web/pages/car.tsx
+++ web/pages/car.tsx
@@ -10,23 +10,58 @@ import {
     ToggleButtonGroup,
     Typography
 } from "@mui/material";
-import {FormEvent, useState} from "react";
+import React, {FormEvent, useState} from "react";
 import Template from "../src/components/Template";
 import {ExpandMore} from "@mui/icons-material";
+import {getTypedItem} from "../src/sessionStorage";
+import {ComparisonType} from "../types/sessionStorageTypes";
 
  const CarSettings: NextPage = () => {
 
      const submitHandler = (event: FormEvent) => {
             event.preventDefault()
-            console.log(`reg=${reg}, vehicle=${vehicle}, fuel=${fuel}`)
+            console.log(`vehicle=${JSON.stringify(vehicle)}, emission=${emission}, distance=${data.distance}`)
      }
 
-     const vehicleTypes = [ "bil", "lastbil", "skåpbil", "combi", "buss", "suv" ]
-     const fuelTypes = [ "bensin", "diesel", "el", "biogas", "etanol" ]
+     const data = getTypedItem<ComparisonType>("comparison", {from: "", to: "", distance: 1})
+
+     const vehicleTypes: {name: string, variants: {name: string, emission: number }[] }[] = [
+         {name: "suv", variants: [{name: "bensin", emission: 0.1694}]},
+         {name: "car", variants: [{name: "bensin", emission: 0.136}, {name: "diesel", emission: 0.119}]},
+         {name: "motorcycle", variants: [{name: "bensin", emission: 0.075}]},
+     ]
 
      const [vehicle, setVehicle] = useState(vehicleTypes[0])
-     const [fuel, setFuel] = useState(fuelTypes[0])
-     const [reg, setReg] = useState("")
+     const [vehicleName, setVehicleName] = useState(vehicle.name)
+     const [emission, setEmission] = useState(vehicleTypes[0].variants[0].emission)
+     const [CO2PerLiter, setCO2PerLiter] = useState<"" | number>("")
+     const [LiterPerKm, setLiterPerKm] = useState<"" | number>("")
+
+     const changeVehicle = (e: React.MouseEvent, v: string) => {
+         if(!v){
+             return
+         }
+
+         console.log(v)
+         setVehicleName(v)
+         const vehicle = vehicleTypes.find(e => e.name === v)
+         if(!vehicle){
+             return
+         }
+         setVehicle(vehicle)
+         setEmission(vehicle.variants[0].emission)
+     }
+
+     const changeEmissionManual = () => {
+         if(!CO2PerLiter || !LiterPerKm){
+             return
+         }
+         setEmission(CO2PerLiter / LiterPerKm)
+     }
+
+     const getTotalEmission = () => {
+            return emission * (data.distance ?? 1)
+     }
 
     return (
         <Template>
@@ -41,13 +76,13 @@ import {ExpandMore} from "@mui/icons-material";
                             <ToggleButtonGroup
                                 size="small"
                                 exclusive
-                                onChange={(e,v) => v ? setVehicle(v): null}
-                                value={vehicle}
+                                onChange={changeVehicle}
+                                value={vehicleName}
                                 color="primary"
                                 fullWidth
                             >
                                 { vehicleTypes.map((vehicleType) => (
-                                    <ToggleButton value={vehicleType}>{vehicleType}</ToggleButton>
+                                    <ToggleButton key={vehicleType.name} value={vehicleType.name}>{vehicleType.name}</ToggleButton>
                                 )) }
                             </ToggleButtonGroup>
                         </Grid>
@@ -56,30 +91,37 @@ import {ExpandMore} from "@mui/icons-material";
                             <ToggleButtonGroup
                                 size="small"
                                 exclusive
-                                onChange={(e,v) => v ? setFuel(v): null}
-                                value={fuel}
+                                onChange={(e,v) => v ? setEmission(v): null}
+                                value={emission}
                                 color="primary"
                                 fullWidth
                             >
-                                { fuelTypes.map((fuelType) => (
-                                    <ToggleButton value={fuelType}>{fuelType}</ToggleButton>
+                                { vehicle.variants.map((fuelType) => (
+                                    <ToggleButton value={fuelType.emission}>{fuelType.name}</ToggleButton>
                                 )) }
                             </ToggleButtonGroup>
                         </Grid>
                         <Grid xs maxWidth="90px">
                             <Typography variant="subtitle2"> Utsläpp: </Typography>
-                            <Typography variant="body2"> 0.0 CO2/km </Typography>
+                            <Typography variant="body2"> {getTotalEmission()} CO2 kg </Typography>
                         </Grid>
                         <Grid xs={12}>
                             <Accordion>
                                 <AccordionSummary expandIcon={<ExpandMore/>}> Avancerat </AccordionSummary>
                                 <AccordionDetails>
                                     <Grid container spacing={1}>
-                                        <Grid xs={12} md={6}>
-                                            <TextField fullWidth label="Koldioxid per Liter" />
+                                        <Grid xs minWidth="200px">
+                                            <TextField fullWidth type="number" value={CO2PerLiter}
+                                                       onChange={e => {setCO2PerLiter(parseFloat(e.target.value)); changeEmissionManual()}}
+                                                       label="KG Koldioxid per Liter" />
                                         </Grid>
-                                        <Grid xs>
-                                            <TextField fullWidth label="Liter per Kilometer" />
+                                        <Grid xs minWidth="200px">
+                                            <TextField fullWidth type="number" label="Liter per Kilometer" value={LiterPerKm}
+                                                       onChange={e => {setLiterPerKm(parseFloat(e.target.value)); changeEmissionManual()}}/>
+                                        </Grid>
+                                        <Grid xs minWidth="200px">
+                                            <TextField fullWidth type="number" value={emission}
+                                                       onChange={(e) => setEmission(parseFloat(e.target.value))} label="KG CO2 per Kilometer" />
                                         </Grid>
                                     </Grid>
                                 </AccordionDetails>
